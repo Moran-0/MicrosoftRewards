213edfe7b235c470bacc709d81aafbb30a6d137e
Moran-0 <2857599224@qq.com>
2023-05-28 11:02:32 +0800

    Changes

--- MicroSoftRewards.py
+++ MicroSoftRewards.py
@@ -1,8 +1,30 @@
 import requests
+import os
 import lxml
-
+import urllib.parse
 if __name__=="__main__":
 
-    url = "https://www.bilibili.com/"
-    text = requests.get(url=url,).text
+    url = "https://cn.bing.com/search?"
+    mobileCookies = 't'
+    with open("mobileCookies.txt",mode='r',encoding='utf-8') as fp:
+        mobileCookies = fp.read().rstrip()
+    print(mobileCookies)
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 10; 16s Pro) AppleWebKit/537.36 '
+                      '(KHTML, like Gecko) Chrome/111.0.0.0 Mobile Safari/537.36',
+        'Cookie':mobileCookies,
+    }
+    searchData = 'listen1'
+    paramData = {
+        'q': searchData
+    }
+    # name = '墨'
+    # location = '南岗区，黑龙江省'
+    # name1 = urllib.parse.quote(name)
+    # location1 = urllib.parse.quote(location)
+    # print(name1)
+    # print(location1)
+    text = requests.get(url=url, headers=headers, params=paramData).text
     print(text)
+
